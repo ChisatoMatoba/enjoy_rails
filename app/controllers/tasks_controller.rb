dfd6cecd1598b9c1de4c2e6dd2181369ae59b5f2
Chisato Matoba <chisato.matoba@luxiar.com>
2024-02-25 20:17:37 +0900

    新規登録

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,24 @@ class TasksController < ApplicationController
   def show
     @task = Task.find_by(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to root_path, notice: '投稿が成功しました。'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task, :task_description)
+  end
 end
